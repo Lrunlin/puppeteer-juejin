3b0fb90091a4766aaf895c55ce1ca4dacdab931d
Lrunlin <liurl0621@gmail.com>
2022-11-02 22:01:57 +0800

    Update getData.ts

--- src/utils/getMoreData/getData.ts
+++ src/utils/getMoreData/getData.ts
@@ -5,8 +5,8 @@ async function loadMoreData(page: Page): Promise<item[]> {
   return (await page.evaluate(() => {
     return new Promise((resolve, reject) => {
       let height = document.documentElement.scrollTop + document.documentElement.clientHeight;
-      // let taggetHeight = height + 4000; //目标高度
-      let taggetHeight = height + 60; //目标高度(测试时使用60)
+      let taggetHeight = height + 4000; //目标高度
+      // let taggetHeight = height + 60; //目标高度(测试时使用60)
       let timer = setInterval(() => {
         window.scrollBy(0, (height += 60));
         if (height >= taggetHeight - 30) {

7a2e0460efd03bd6a3577dc86cdf5b915248770a
Lrunlin <liurl0621@gmail.com>
2022-12-20 21:54:26 +0800

    ✨ feat(根据环境变量判断滚动高度):

--- src/utils/getMoreData/getData.ts
+++ src/utils/getMoreData/getData.ts
@@ -1,11 +1,15 @@
 import type { Page } from "puppeteer";
 /** 加载更多信息并返回列表*/
 async function loadMoreData(page: Page): Promise<string[]> {
+  // 通过sessionStorage设置传递环境变量
+  (await page.target().createCDPSession()).send("Runtime.evaluate", {
+    expression: `sessionStorage.setItem('env', '${process.env.ENV}')`,
+  });
   return (await page.evaluate(() => {
     return new Promise((resolve, reject) => {
       let height = document.documentElement.scrollTop + document.documentElement.clientHeight;
-      let taggetHeight = height + 4000; //目标高度(测试时使用60)
-      
+      let taggetHeight = height + (sessionStorage.getItem("env") == "development" ? 60 : 4000); //目标高度
+
       let timer = setInterval(() => {
         window.scrollBy(0, (height += 60));
         if (height >= taggetHeight - 90) {
